b981db86afbae8ef725dffa3ac1b227f1196622a
Pedro Papini <ppapinia@gmail.com>
2022-07-28 14:59:51 -0300

    Configuração inicial do Redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,10 @@
 import { createStore } from 'react-redux';
+import { applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 
-const store = createStore();
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 if (window.Cypress) {
   window.store = store;
