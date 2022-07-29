63519150033b4bd25bc9a163bdd87bb7fd04398a
Pedro Papini <ppapinia@gmail.com>
2022-07-29 17:18:36 -0300

    Resolve requisito 4 e Startest 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,16 @@ import { connect } from 'react-redux';
 
 class Header extends Component {
   render() {
-    const { email } = this.props;
+    const { email, expenses } = this.props;
+    const totalExpensesValue = expenses.reduce((acc, cur) => {
+      const { currency, value, exchangeRates } = cur;
+      const rate = parseFloat(exchangeRates[currency].ask);
+      return acc + parseFloat(value) * rate;
+    }, 0);
     return (
       <div>
         <p data-testid="email-field">{email}</p>
-        <p data-testid="total-field">0</p>
+        <p data-testid="total-field">{totalExpensesValue.toFixed(2)}</p>
         <p data-testid="header-currency-field">BRL</p>
       </div>
     );
@@ -17,10 +22,12 @@ class Header extends Component {
 
 const mapStateToProps = (store) => ({
   email: store.user.email,
+  expenses: store.wallet.expenses,
 });
 
 Header.propTypes = {
   email: PropTypes.string.isRequired,
+  expenses: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default connect(mapStateToProps)(Header);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,4 +1,4 @@
-import USER_LOGIN, { SAVE_CURRENCIES } from './actionTypes';
+import USER_LOGIN, { SAVE_CURRENCIES, SAVE_EXPENSES } from './actionTypes';
 
 const loginUser = (email) => ({
   type: USER_LOGIN,
@@ -10,11 +10,19 @@ export const saveCurrencies = (currencies) => ({
   payload: currencies,
 });
 
-export function fetchCurrencies() {
+export const saveExpenses = (expenses) => ({
+  type: SAVE_EXPENSES,
+  payload: expenses,
+});
+
+export function requestFromAPI(newExpense) {
   return async (dispatch) => {
     const endpoint = 'https://economia.awesomeapi.com.br/json/all';
     const request = await fetch(endpoint);
     const data = await request.json();
+    if (newExpense) {
+      return dispatch(saveExpenses({ ...newExpense, exchangeRates: data }));
+    }
     const currencies = Object
       .keys(data)
       .filter((currency) => currency !== 'USDT');
