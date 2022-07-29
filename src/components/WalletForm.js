f61e763574e620463dfaf059172bab59525cada0
Pedro Papini <ppapinia@gmail.com>
2022-07-28 21:01:26 -0300

    Resolve requisito 3 e Startest 3

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -1,9 +1,121 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchCurrencies } from '../redux/actions';
+import Input from './Input';
 
-export default class WalletForm extends Component {
+class WalletForm extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: '',
+      description: '',
+      currency: 'USD',
+      method: 'Dinheiro',
+      tag: 'Alimentação',
+    };
+  }
+
+  async componentDidMount() {
+    const { getCurrencies } = this.props;
+    getCurrencies();
+    // setCurrencies(currencies);
+  }
+
+  fetchCurrencies = async () => {
+    const endpoint = 'https://economia.awesomeapi.com.br/json/all';
+    const request = await fetch(endpoint);
+    const data = await request.json();
+    const currencies = Object
+      .keys(data)
+      .filter((currency) => currency !== 'USDT');
+    return currencies;
+  }
+
+  handleInput = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const {
+      value,
+      description,
+      currency,
+      method,
+      tag,
+    } = this.state;
+    const { storedCurrencies } = this.props;
     return (
-      <div>WalletForm</div>
+      <form>
+        <Input
+          name="value"
+          type="number"
+          placeholder="Valor"
+          dataTestId="value-input"
+          value={ value }
+          onChange={ this.handleInput }
+        />
+        <Input
+          name="description"
+          type="text"
+          placeholder="Descrição"
+          dataTestId="description-input"
+          value={ description }
+          onChange={ this.handleInput }
+        />
+        <select
+          name="currency"
+          value={ currency }
+          data-testid="currency-input"
+          onChange={ this.handleInput }
+        >
+          {
+            storedCurrencies
+              .map((ticker) => <option key={ ticker } value={ ticker }>{ticker}</option>)
+          }
+        </select>
+        <select
+          name="method"
+          value={ method }
+          data-testid="method-input"
+          onChange={ this.handleInput }
+        >
+          <option value="Dinheiro">Dinheiro</option>
+          <option value="Cartão de crédito">Cartão de crédito</option>
+          <option value="Cartão de débito">Cartão de débito</option>
+        </select>
+        <select
+          name="tag"
+          value={ tag }
+          data-testid="tag-input"
+          onChange={ this.handleInput }
+        >
+          <option value="Alimentação">Alimentação</option>
+          <option value="Lazer">Lazer</option>
+          <option value="Trabalho">Trabalho</option>
+          <option value="Transporte">Transporte</option>
+          <option value="Saúde">Saúde</option>
+        </select>
+      </form>
     );
   }
 }
+
+const mapDispatchToProps = (dispatch) => ({
+  getCurrencies: () => dispatch(fetchCurrencies()),
+});
+
+const mapStateToProps = (store) => ({
+  storedCurrencies: store.wallet.currencies,
+});
+
+WalletForm.propTypes = {
+  getCurrencies: PropTypes.func.isRequired,
+  storedCurrencies: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(WalletForm);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,8 +1,25 @@
-import USER_LOGIN from './actionTypes';
+import USER_LOGIN, { SAVE_CURRENCIES } from './actionTypes';
 
 const loginUser = (email) => ({
   type: USER_LOGIN,
   payload: email,
 });
+
+export const saveCurrencies = (currencies) => ({
+  type: SAVE_CURRENCIES,
+  payload: currencies,
+});
+
+export function fetchCurrencies() {
+  return async (dispatch) => {
+    const endpoint = 'https://economia.awesomeapi.com.br/json/all';
+    const request = await fetch(endpoint);
+    const data = await request.json();
+    const currencies = Object
+      .keys(data)
+      .filter((currency) => currency !== 'USDT');
+    dispatch(saveCurrencies(currencies));
+  };
+}
 
 export default loginUser;
