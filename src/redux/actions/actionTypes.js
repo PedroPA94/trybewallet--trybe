5e0f64aeacb5456f13ed266137b1b03c1bf4de2c
Pedro Papini <ppapinia@gmail.com>
2022-08-01 17:39:48 -0300

    Corrige importação de action type

--- src/redux/actions/actionTypes.js
+++ src/redux/actions/actionTypes.js
@@ -1,4 +1,4 @@
-export const USER_LOGIN = 'USER_LOGIN';
+const USER_LOGIN = 'USER_LOGIN';
 export const SAVE_CURRENCIES = 'SAVE_CURRENCIES';
 export const SAVE_EXPENSE = 'SAVE_EXPENSE';
 export const DELETE_EXPENSE = 'DELETE_EXPENSE';
